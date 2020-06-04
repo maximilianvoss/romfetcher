a80a7859fdaef945fc934c671226bcab41d33f37
Maximilian Voss <voss@adobe.com>
2020-06-04 11:29:27 +0200

    In which our hero did some small improvements

--- src/database/postprocess.c
+++ src/database/postprocess.c
@@ -59,7 +59,7 @@ static void fillDefaultValus(sqlite3 *db) {
 
     char *query = "INSERT INTO postprocessors (filesuffix, command, active) VALUES\
                         ('.zip', 'unzip -o \"%file%\" -d \"%target%\"', 0),\
-                        ('.rar', 'unrar -x -f \"%file%\ \"%target%\"', 1),\
+                        ('.rar', 'unrar -x -f \"%file%\" \"%target%\"', 1),\
                         ('.7z', '7zr -y x \"%file%\" -w \"%target%\"', 1)";
     int rc = sqlite3_exec(db, query, 0, 0, &err_msg);
     if (rc != SQLITE_OK) {
